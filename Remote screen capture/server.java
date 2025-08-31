dcc40e9f3fe3b724ce685ff3283f20562be0e0c6
Thujaina <141035060+Thujaina@users.noreply.github.com>
2025-08-31 23:47:12 +0530

    Create server.java

---
+++ Remote screen capture/server.java
@@ -0,0 +1,29 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import javax.imageio.ImageIO;
+
+public class ScreenServer {
+    public static void main(String[] args) throws Exception {
+        ServerSocket server = new ServerSocket(5000);
+        System.out.println("Waiting for client...");
+        Socket socket = server.accept();
+        System.out.println("Client connected.");
+
+        // Capture screenshot
+        Robot robot = new Robot();
+        Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+        BufferedImage screenshot = robot.createScreenCapture(screenRect);
+
+        // Send screenshot to client
+        OutputStream out = socket.getOutputStream();
+        ImageIO.write(screenshot, "png", out);
+
+        out.close();
+        socket.close();
+        server.close();
+        System.out.println("Screenshot sent.");
+    }
+}
