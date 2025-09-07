2cf98b183ff1f19d7b8c423fab23109540d8bc2c
Thujaina <141035060+Thujaina@users.noreply.github.com>
2025-09-07 22:18:44 +0530

    Create client.java

---
+++ one way TCP/client.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.net.*;
+public class Client 
+{
+    public static void main(String[] args) throws IOException 
+   {
+        Socket s = new Socket("localhost", 5000);
+        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        String msg;
+        while (!(msg = in.readLine()).equals("exit")) 
+       {
+            System.out.println("From Server: " + msg);
+        }
+        s.close();
+    }
+}
